fb9879216379d17c2db488081451054cd6e5bbdf
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-08-02 13:03:26 +0300

    add Summary page for cart

---
+++ Rocky/Rocky/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Rocky.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+
+        public ApplicationUser Application { get; set; }
+        public IEnumerable<Product> ProductList { get; set; }
+    }
+}
