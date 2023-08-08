5810503489e8c185fd23a319e311bd2e82f85b4e
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-08-08 23:32:29 +0300

    Create Interface and Repository

---
+++ Rocky/Rocky_DatAccess/Repository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rocky_DatAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky_DatAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(AppDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+'
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Find(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> filter = null, string includePropreties = null, bool isTracing = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includePropreties = null, bool isTracing = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
+    
