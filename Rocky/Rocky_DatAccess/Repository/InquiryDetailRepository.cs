95c8dce1f83feb9e64f36d4033b09b973fbc363d
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-08-09 22:20:30 +0300

    Add IInquiryHeader and Detail Repository

---
+++ Rocky/Rocky_DatAccess/Repository/InquiryDetailRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Rocky_DatAccess.Repository.IRepository;
+using Rocky_Models;
+using Rocky_Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rocky_DatAccess.Repository
+{
+    public class InquiryDetailRepository : Repository<InquiryDetail>, IInquiryDetailRepository
+    {
+        private readonly AppDbContext _db;
+
+        public InquiryDetailRepository(AppDbContext db): base(db)
+        {
+            _db = db;       
+        }
+
+        public void Update(InquiryDetail obj)
+        {
+            _db.InquiryDetail.Update(obj);
+        }
+    }
+}
