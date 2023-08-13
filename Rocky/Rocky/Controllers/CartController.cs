a388d6780a23e630a0103a52dc5218bfe65ff723
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-08-13 19:32:59 +0300

    added scripts for admin in cart

--- Rocky/Rocky/Controllers/CartController.cs
+++ Rocky/Rocky/Controllers/CartController.cs
@@ -129,8 +129,14 @@ namespace Rocky.Controllers
             ProductUserVM = new ProductUserVM()
             {
                 ApplicationUser = applicationUser,
-                ProductList = prodList.ToList()
             };
+
+            foreach (var cartObj in shoppingCartsList) 
+            {
+                Product prodTemp = _prodRepo.FirstOrDefault(u => u.Id == cartObj.ProductId);
+                prodTemp.TempSqFt = cartObj.SqFt;
+                ProductUserVM.ProductList.Add(prodTemp);
+            }
 
             return View(ProductUserVM);
         }
--- Rocky/Rocky_Models/ApplicationUser.cs
+++ Rocky/Rocky_Models/ApplicationUser.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Rocky_Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public  string FullName { get; set; }
+
+        [NotMapped]
+        public string StreetAddress { get; set; }
+
+        [NotMapped]
+        public string City { get; set; }
+
+        [NotMapped]
+        public string State { get; set; }
+
+        [NotMapped]
+        public string PostalCode { get; set; }
     }
 }
